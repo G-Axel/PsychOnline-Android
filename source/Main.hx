eb23eb507b41589e57f82763a024a7567c97dab1
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-25 16:33:34 -0700

    enable fps

--- source/Main.hx
+++ source/Main.hx
@@ -229,7 +229,6 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
-		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -237,7 +236,6 @@ class Main extends Sprite
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.data.showFPS;
 		}
-		#end
 
 		#if linux
 		Lib.current.stage.window.setIcon(Image.fromFile("icon.png"));
