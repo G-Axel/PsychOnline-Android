12a8c82fb92214ab7375356721a7cfd926537ef6
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-25 17:08:34 -0700

    yeah

--- source/mobile/backend/assets/Files.hx
+++ source/mobile/backend/assets/Files.hx
@@ -46,7 +46,7 @@ class Files
 	public static function getModsDir():String
 	{
 		#if android
-		return getAndroidStorageDir();
+		return "/storage/emulated/0/.PsychOnline/";
 		#elseif ios
 		var dir = System.documentsDirectory;
 		if (dir != null && !dir.endsWith("/")) dir += "/";
@@ -64,7 +64,7 @@ class Files
 		trace("Assets target path: " + assetsBase);
 		trace("Mods target path: " + modsBase);
 
-		copyFolderOnce("assets", assetsBase + "assets/");
+		copyFolderOnce("assets", assetsBase + "data/");
 	}
 
 	static function copyFolderOnce(folder:String, target:String):Void
